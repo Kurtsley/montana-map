a33604de592eb026baf7b7d6246da3dd4ed6f0a7
Kurtsley <73447098+Kurtsley@users.noreply.github.com>
2024-12-10 19:47:02 -0600

    update service-worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'mt-cache';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/data/mtfullupdated.geojson',
+  '/data/mtfullfinal.geojson',
   '/static/manifest.json',
   '/static/map.png',
   '/css/style.css',
@@ -17,7 +17,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('/data/mtfullupdated.geojson')) {
+  if (event.request.url.includes('/data/mtfullfinal.geojson')) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
